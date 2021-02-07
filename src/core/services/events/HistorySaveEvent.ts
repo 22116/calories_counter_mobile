bb72d7e5d00ce735a5b38adbdda27225fc167357
victor.fedorenko <victor.fedorenko@mgid.com>
2021-02-07 22:42:58 +0200

    Enhanced HistorySaveEvent.ts

--- src/core/services/events/HistorySaveEvent.ts
+++ src/core/services/events/HistorySaveEvent.ts
@@ -1,7 +1,6 @@
 import { HistoryService } from 'src/core/services/HistoryService'
-import { Counter, History } from 'src/core/entities'
+import { Counter } from 'src/core/entities'
 import { Score } from 'src/core/entities/Counter'
-import { IdGenerator } from 'src/utility/encryption'
 import { HistoryRepository } from 'src/core/repositories'
 import { inject, injectable } from 'tsyringe'
 import { EventHandler, Event } from 'src/core/services/events/models'
@@ -18,20 +17,14 @@ export class HistorySaveEventHandler implements EventHandler {
   ) { }
 
   async handle(event: HistorySaveEvent) {
-    let history = await this.historyRepository.findByDateAndCounterId(event.date, event.counter.id)
+    const history = await this.historyRepository.findByDateAndCounterId(event.date, event.counter.id)
 
     if (history !== null) {
       history.scores = event.counter.scores
 
       await this.historyRepository.update(history)
     } else {
-      history = new History()
-      history.id = new IdGenerator().generate()
-      history.counter_id = event.counter.id
-      history.date = event.date
-      history.scores = event.counter.scores
-
-      await this.historyRepository.save(history)
+      await this.historyRepository.save(this.historyService.create(event.counter))
     }
   }
 
